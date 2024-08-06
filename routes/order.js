56169180821cf678a220c365d741df0e0348069c
ThiagoAndo <ando.thiago@gmail.com>
2024-08-06 09:53:06 +0100

    Testing render3

--- routes/order.js
+++ routes/order.js
@@ -8,32 +8,23 @@ const { isName, isEmail } = require("../helpers/validate");
 const { isCorret } = require("../helpers/validate");
 
 router.post("/", (req, res) => {
-  let bodylen;
-  if (req.body?.cart) {
-    bodylen = 4;
-  } else {
-    bodylen = 3;
+  const id = req.body.id;
+  const name = req.body.name;
+  const email = req.body.email;
+  const cart = req.body?.cart || false;
+  if (!isName(name)) {
+    res.status(500).json({
+      message: "Name is wrong. Make sure to enter first and last name only",
+    });
+    return;
+  } else if (!isEmail(email)) {
+    res.status(500).json({ message: "Email is not valid" });
+    return error;
   }
-
-  if (isCorret(bodylen, req.body)) {
-    const id = req.body.id;
-    const name = req.body.name;
-    const email = req.body.email;
-    const cart = req.body?.cart || false;
-    if (!isName(name)) {
-      res.status(500).json({
-        message: "Name is wrong. Make sure to enter first and last name only",
-      });
-      return;
-    } else if (!isEmail(email)) {
-      res.status(500).json({ message: "Email is not valid" });
-      return error;
-    }
-    const ret = insertOrder(id, name, email, cart);
-    ret.changes > 0
-      ? res.status(201).json({ message: "Invoice created" })
-      : res.status(500).json({ message: "An error has occurred" });
-  }
+  const ret = insertOrder(id, name, email, cart);
+  ret.changes > 0
+    ? res.status(201).json({ message: "Invoice created" })
+    : res.status(500).json({ message: "An error has occurred" });
 });
 router.use(checkAuth);
 router.get("/:id", (req, res) => {
