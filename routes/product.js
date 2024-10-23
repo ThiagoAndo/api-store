70c37a89e417fe9c49e5ca31fed0915b7276f6a9
ThiagoAndo <ando.thiago@gmail.com>
2024-10-23 17:10:01 +0100

    Setup cron-job.org

--- routes/product.js
+++ routes/product.js
@@ -11,6 +11,16 @@ router.get("/", async (req, res) => {
   res.status(200).json({ products, images });
 });
 
+router.get("/byid/:id", async (req, res) => {
+  const id = req.params.id;
+  const products = readAction("products", "id = ?", [id]);
+  products?.length
+    ? res.status(200).json({ products })
+    : res
+        .status(404)
+        .json({ message: `Could not find product with id: ${id}` });
+});
+
 router.use(checkAuth);
 
 module.exports = router;
